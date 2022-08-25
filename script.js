c515c8eea35d979b3533bff9b7ad4789ac142d13
Rodrigo <rodrigo.rpereira1998@gmail.com>
2022-08-25 23:00:03 +0100

    add click to add crosses and circles functionality

--- script.js
+++ script.js
@@ -1,17 +1,49 @@
 const boardSpaces = document.querySelectorAll(".board-space")
 
-const GameBoard = (() => {
+const game = (() => {
+    let turn = 1
+    let players = []
+
     const gameBoard = [
-        "x", "x", "x", 
-        "o", "o", "x", 
-        "o", "o", "x"
+        " ", " ", " ", 
+        " ", " ", " ", 
+        " ", " ", " "
     ]
 
-    return {gameBoard}
+    const endTurn = () => {
+        if(turn === 1) {
+            turn = 2
+            console.log(turn)
+        }
+        else if(turn === 2) {
+            turn = 1
+            console.log(turn)
+        }
+    }
+
+    const addMarker = (index) => {
+        let marker = ""
+        if(turn === 1)
+            marker = "x"
+        else
+            marker = "o"
+
+        if(gameBoard[index] === " ") {
+            gameBoard[index] = marker
+            endTurn()
+        }
+    }
+
+    return {gameBoard, addMarker}
 })()
 
-const Player = () => {
+const Player = (name, marker) => {
+    let playerName = name
+    let playerMarker = marker
+    let score = 0
+    let addPoint = () => score += 1
 
+    return {playerName, playerMarker, score}
 }
 
 const displayController = (() => {
@@ -21,8 +53,18 @@ const displayController = (() => {
         }
     }
 
-    return {updateDisplay}
+    const updateMarkers = (e) => {
+        let boardSpace = parseInt(e.target.id)
+        game.addMarker(boardSpace)
+        displayController.updateDisplay(boardSpaces, game.gameBoard)
+    }
+
+    return {updateDisplay, updateMarkers}
 })()
 
-displayController.updateDisplay(boardSpaces, GameBoard.gameBoard)
 
+
+
+boardSpaces.forEach(element => {
+    element.addEventListener("click", displayController.updateMarkers)
+});
